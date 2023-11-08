ad11e6298dd46254530c6b0b511a42e4d065202b
ghazibentini <ghazi.b17@gmail.com>
2023-11-08 15:40:49 +0100

    initial setup

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./navbar.scss"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        {/* Sidebar */}
+        <div className="wrapper">
+            <span>Ghazi Bentini</span>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
